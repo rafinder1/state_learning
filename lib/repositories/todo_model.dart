1e0cedb6796bc6e6f075865b27ef82ad3895438c
rafinder1 <rafpro33@gmail.com>
2024-04-19 14:43:21 +0200

    implement freezed in todo_model

--- lib/repositories/todo_model.dart
+++ lib/repositories/todo_model.dart
@@ -1,25 +1,65 @@
-import 'dart:convert';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-class Todo {
-  
-    int userId;
-    int id;
-    String title;
-    bool completed;
+part 'todo_model.freezed.dart';
+part 'todo_model.g.dart';
 
-    Todo({
-        required this.userId,
-        required this.id,
-        required this.title,
-        required this.completed,
-    });
 
-    factory Todo.fromRawJson(String str) => Todo.fromJson(json.decode(str));
+@freezed
+class Todo with _$Todo {
+  const factory Todo({
+    int? userId,
+    int? id,
+    String? title,
+    bool? completed,
+  }) = _Todo;
 
-    factory Todo.fromJson(Map<String, dynamic> json) => Todo(
-        userId: json["userId"],
-        id: json["id"],
-        title: json["title"],
-        completed: json["completed"],
-    );
+  factory Todo.fromJson(Map<String, dynamic> json) => _$TodoFromJson(json);
 }
+
+// class Todo {
+  
+//     int userId;
+//     int id;
+//     String title;
+//     bool completed;
+
+//     Todo({
+//         required this.userId,
+//         required this.id,
+//         required this.title,
+//         required this.completed,
+//     });
+
+//     factory Todo.fromRawJson(String str) => Todo.fromJson(json.decode(str));
+
+//     factory Todo.fromJson(Map<String, dynamic> json) => Todo(
+//         userId: json["userId"],
+//         id: json["id"],
+//         title: json["title"],
+//         completed: json["completed"],
+//     );
+// }
+
+// class Todo {
+  
+//     int userId;
+//     int id;
+//     String title;
+//     bool completed;
+
+//     Todo({
+//         required this.userId,
+//         required this.id,
+//         required this.title,
+//         required this.completed,
+//     });
+
+//     factory Todo.fromRawJson(String str) => Todo.fromJson(json.decode(str));
+
+//     factory Todo.fromJson(Map<String, dynamic> json) => Todo(
+//         userId: json["userId"],
+//         id: json["id"],
+//         title: json["title"],
+//         completed: json["completed"],
+//     );
+// }
