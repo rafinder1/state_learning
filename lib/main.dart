56e59ad797427ae09d6056f68d326e5a30390919
rafinder1 <102503112+rafinder1@users.noreply.github.com>
2024-04-19 09:59:46 +0200

    Info about StateFullWidget (#1)
    * extract home page as class
    
    * added variable number which show on page - you can only change this in code
    
    * convert to StateFulWidget and add a ElevatedButton
    
    * changed number in setState
    
    * added repository with fetchData

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,67 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(),
+      home: const HomePage(),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int number = 10;
+  String? errorMessage;
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(errorMessage ?? ''),
+            Text(
+              '$number',
+              style: const TextStyle(fontSize: 40),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                setState(() {
+                  isLoading = true;
+                  errorMessage = null;
+                });
+                final repo = FakeRepository();
+                final result = await repo.fetchData();
+                setState(() {
+                  number += 1; 
+                  errorMessage = result;
+                  isLoading = false;
+                });
+              },
+              child: const Text('Jazda w to!'),
+            ),
+            if (isLoading) const CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FakeRepository {
+  Future<String> fetchData() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return 'KLASA';
   }
 }
