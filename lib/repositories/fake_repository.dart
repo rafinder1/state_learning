579c6c9822f0d340e5ebf29d6adc2715a2ee77c3
rafinder1 <rafpro33@gmail.com>
2024-04-19 14:44:25 +0200

    changed method serialization after get data from http response

--- lib/repositories/fake_repository.dart
+++ lib/repositories/fake_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:state_learning/repositories/todo_model.dart';
 
@@ -6,8 +8,8 @@ class FakeRepository {
     final url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
     final response = await http.get(url);
 
-    final todoModel = Todo.fromRawJson(response.body);
+    final todoModel = Todo.fromJson(json.decode(response.body));
 
-    return todoModel.title ?? 'nie maaa';
+    return todoModel.title ?? 'nie maa';
   }
 }
