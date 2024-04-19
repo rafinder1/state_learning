6396e4970cb0f1743a5a693b90f5caa489d1ac5a
rafinder1 <rafpro33@gmail.com>
2024-04-19 13:34:52 +0200

    create a new repo

---
+++ lib/repositories/fake_repository.dart
@@ -0,0 +1,13 @@
+import 'package:http/http.dart' as http;
+import 'package:state_learning/repositories/todo_model.dart';
+
+class FakeRepository {
+  Future<String> fetchData() async {
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
+    final response = await http.get(url);
+
+    final todoModel = Todo.fromRawJson(response.body);
+
+    return todoModel.title ?? 'nie maaa';
+  }
+}
