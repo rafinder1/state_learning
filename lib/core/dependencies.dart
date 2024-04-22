1dabe5c1f6a1c0c278e964c273ceb4c4972e01d4
rafinder1 <rafpro33@gmail.com>
2024-04-22 08:25:39 +0200

    changed get it in injectable

--- lib/core/dependencies.dart
+++ lib/core/dependencies.dart
@@ -1,12 +1,12 @@
 import 'package:get_it/get_it.dart';
-import 'package:state_learning/cubit/home_cubit.dart';
-import 'package:state_learning/datasources/remote_data_source.dart';
-import 'package:state_learning/repositories/fake_repository.dart';
+import 'package:injectable/injectable.dart';
+import 'package:state_learning/core/dependencies.config.dart';
 
-final getIt = GetIt.instance;
-
-void configureDependencies() {
-  getIt.registerLazySingleton<RemoteDataSource>(() => RemoteDataSource());
-  getIt.registerLazySingleton<FakeRepository>(() => FakeRepository(getIt()));
-  getIt.registerLazySingleton<HomeCubit>(() => HomeCubit(getIt()));
-}
+final getIt = GetIt.instance;  
+  
+@InjectableInit(  
+  initializerName: 'init', // default  
+  preferRelativeImports: true, // default  
+  asExtension: true, // default  
+)  
+void configureDependencies() => getIt.init();  
