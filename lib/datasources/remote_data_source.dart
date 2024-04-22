b7b9e93e65bbb6e352d47469c17d18ca5ed7111e
rafinder1 <rafpro33@gmail.com>
2024-04-22 08:26:30 +0200

    added decorators injectable and lazySingleton

--- lib/datasources/remote_data_source.dart
+++ lib/datasources/remote_data_source.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:injectable/injectable.dart';
 import 'package:state_learning/repositories/todo_model.dart';
 
+@lazySingleton
 class RemoteDataSource {
   Future<String> fetchData() async {
     final url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
