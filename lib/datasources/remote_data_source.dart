7b6f485fef6852e38fcbe8bc3926046b98cc1361
rafinder1 <rafpro33@gmail.com>
2024-04-22 07:59:51 +0200

    added remote data source

---
+++ lib/datasources/remote_data_source.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:state_learning/repositories/todo_model.dart';
+
+class RemoteDataSource {
+  Future<String> fetchData() async {
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
+    final response = await http.get(url);
+
+    final todoModel = Todo.fromJson(json.decode(response.body));
+
+    return todoModel.title ?? 'nie maa';
+  }
+}
